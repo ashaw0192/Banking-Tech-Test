9201e1e7066300ad83da4350e8ad756d49567f45
Anthony Shaw <ashaw0192@gmail.com>
2023-01-23 16:42:54 +0000

    refactored the Model Class and tests

--- bankModel.js
+++ bankModel.js
@@ -14,21 +14,28 @@ class BankModel {
 
   deposit(amount) {
     this.balance += amount;
-    this.formatTransactionList('debit', amount)
+    this.formatTransactionList("debit", amount);
   }
 
   withdraw(amount) {
     this.balance -= amount;
-    this.formatTransactionList('credit', amount)
+    this.formatTransactionList("credit", amount);
   }
 
-  formatTransactionList(type, amount) {
-    let date = new Date();
-    let transactionObj ={}
-    transactionObj["date"] = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-    transactionObj[type] = amount;
+  formatTransactionList(transactionType, amount) {
+    let transactionObj = {};
+    transactionObj["date"] = this.formatDate();
+    transactionObj[transactionType] = amount;
     transactionObj["balance"] = this.balance;
     this.transactionList.push(transactionObj);
+  }
+
+  formatDate() {
+    let date = new Date();
+    const day = ("0" + date.getDate()).slice(-2);
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
   }
 }
 
--- bankModel.test.js
+++ bankModel.test.js
@@ -13,6 +13,16 @@ describe("BankModel", () => {
     expect(model.getTransactions()).toEqual([]);
   });
 
+  it("formats the date", () => {
+    const model = new BankModel();
+    let date = new Date();
+    const day = ("0" + date.getDate()).slice(-2);
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+
+    expect(model.formatDate()).toEqual(`${day}/${month}/${year}`);
+  })
+
   it("returns a balance of 100.00 when 100.00 is deposited", () => {
     const model = new BankModel();
     model.deposit(100.0);
@@ -23,11 +33,11 @@ describe("BankModel", () => {
   it("reflects in the transaction list when deposit made", () => {
     const model = new BankModel();
     model.deposit(100.0);
-    let date = new Date();
+    let date = model.formatDate();
 
     expect(model.getTransactions()).toEqual([
       {
-        date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
+        date: date,
         debit: 100.0,
         balance: 100.0,
       },
@@ -37,73 +47,73 @@ describe("BankModel", () => {
   it("reflects in the transaction list when multiple deposits made", () => {
     const model = new BankModel();
     model.deposit(100.05);
-    model.deposit(50.50)
-    let date = new Date();
+    model.deposit(50.5);
+    let date = model.formatDate();
 
     expect(model.getTransactions()).toEqual([
       {
-        date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
+        date: date,
         debit: 100.05,
         balance: 100.05,
       },
       {
-        date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
-        debit: 50.50,
+        date: date,
+        debit: 50.5,
         balance: 150.55,
       },
     ]);
-  })
+  });
 
   it("returns a balance of -100.00 when 100.00 withdrawn", () => {
     const model = new BankModel();
-    model.withdraw(100.00);
+    model.withdraw(100.0);
 
-    expect(model.getBalance()).toEqual(-100.00);
+    expect(model.getBalance()).toEqual(-100.0);
   });
 
   it("reflects in transaction list when withdrawal made", () => {
     const model = new BankModel();
-    model.withdraw(100.00);
-    let date = new Date();
+    model.withdraw(100.0);
+    let date = model.formatDate();
 
     expect(model.getTransactions()).toEqual([
       {
-        date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
+        date: date,
         credit: 100.0,
         balance: -100.0,
       },
     ]);
-  })
+  });
 
   it("reflects in the transaction list with multiple deposit/withdrawal", () => {
     const model = new BankModel();
-    model.deposit(50.00);
-    model.withdraw(45.00);
-    model.deposit(40.00);
+    model.deposit(50.0);
+    model.withdraw(45.0);
+    model.deposit(40.0);
     model.withdraw(5.55);
-    let date = new Date();
+    let date = model.formatDate();
 
     expect(model.getTransactions()).toEqual([
       {
-        date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
-        debit: 50.00,
-        balance: 50.00,
+        date: date,
+        debit: 50.0,
+        balance: 50.0,
       },
       {
-        date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
-        credit: 45.00,
-        balance: 5.00,
+        date: date,
+        credit: 45.0,
+        balance: 5.0,
       },
       {
-        date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
-        debit: 40.00,
-        balance: 45.00,
+        date: date,
+        debit: 40.0,
+        balance: 45.0,
       },
       {
-        date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
+        date: date,
         credit: 5.55,
         balance: 39.45,
-      }
-    ])
-  })
+      },
+    ]);
+  });
 });
